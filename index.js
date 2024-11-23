70f0cb05150ad7b146453e9438a1d8c71d615445
joey-moon <imshubhamshah@gmail.com>
2024-11-23 23:42:17 +0530

    committed

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 const flames = document.querySelectorAll('.flame');
 
 // Add a click event listener to each flame
+let i=0;
 flames.forEach((flame) => {
     flame.addEventListener('click', () => {
+        i++;
         flame.style.animation = 'none'; // Stop flicker animation
         flame.style.transition = 'opacity 0.5s ease'; // Add smooth fade-out
         flame.style.opacity = '0'; // Fade out the flame
+        if(i>=3){
+            audio.play();
+        }
     });
 });
