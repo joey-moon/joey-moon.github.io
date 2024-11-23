6052a40b49daf5f8a4a827dbe47475118f595e55
joey-moon <imshubhamshah@gmail.com>
2024-11-24 00:34:33 +0530

    commit

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
 const flames = document.querySelectorAll('.flame');
 
 // Add a click event listener to each flame
-let i=0;
+let blownOutCount = 0;
 flames.forEach((flame) => {
     flame.addEventListener('click', () => {
-        i++;
-        flame.style.animation = 'none'; // Stop flicker animation
-        flame.style.transition = 'opacity 0.5s ease'; // Add smooth fade-out
-        flame.style.opacity = '0'; // Fade out the flame
-        if(i>=3){
-            audio.play();
-            muteButton.innerHTML = '&#x1F50A;';
+        if (flame.style.opacity !== '0') {
+            blownOutCount++;
+            flame.style.animation = 'none'; // Stop flicker animation
+            flame.style.transition = 'opacity 0.5s ease'; // Add smooth fade-out
+            flame.style.opacity = '0'; // Fade out the flame
+            if(blownOutCount===flames.length){
+                audio.play();
+                muteButton.innerHTML = '&#x1F50A;';
+            }
         }
     });
 });
