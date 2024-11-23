e39e01281d4e167a05b793ae806c943d4c200d67
joey-moon <imshubhamshah@gmail.com>
2024-11-24 01:31:21 +0530

    commit

--- index.js
+++ index.js
@@ -15,7 +15,30 @@ flames.forEach((flame) => {
                 audio.play();
                 muteButton.innerHTML = '&#x1F50A;';
                 container.classList.add('fade-in');
+                setTimeout(launchConfetti, 500);  // Confetti starts immediately
+                setTimeout(launchFireworks, 1000); 
             }
         }
     });
 });
+
+function launchConfetti() {
+    confetti({
+        particleCount: 500, // Number of confetti particles
+        spread: 70,         // Spread angle
+        origin: { x: 0.5, y: 0.5 } // Confetti starts from center
+    });
+}
+
+function launchFireworks() {
+    const overlay = document.getElementById('fireworks-overlay'); // Get the overlay
+    const fireworks = new Fireworks(overlay, {
+        rocketsPoint: 50, // Control spread of rockets
+        hue: { min: 0, max: 360 }, // Color range
+        speed: 2, // Animation speed
+        acceleration: 1.05, // Speeding up effect
+    });
+
+    fireworks.start();
+    // setTimeout(() => fireworks.stop(), 5000); // Stop after 5 seconds
+}
