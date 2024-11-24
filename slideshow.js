3864ba8c7bc221f7d546c159a8ad27f3e37a0181
joey-moon <imshubhamshah@gmail.com>
2024-11-24 14:15:27 +0530

    commit

--- slideshow.js
+++ slideshow.js
@@ -1,3 +1,21 @@
+const audio = document.getElementById('background-music');
+const muteButton = document.getElementById('mute-btn');
+
+// Initial state: audio is playing, button shows speaker icon
+let isMuted = false;
+
+// Toggle mute/unmute when the button is clicked
+muteButton.addEventListener('click', () => {
+    if (isMuted) {
+        audio.play();  // Play audio if muted
+        muteButton.innerHTML = '&#x1F50A;';  // Speaker icon
+    } else {
+        audio.pause();  // Pause audio if playing
+        muteButton.innerHTML = '&#x1F507;';  // Mute icon
+    }
+    isMuted = !isMuted;  // Toggle the mute state
+});
+
 const images = ["images/photo.png", "images/photo1.png", "images/photo2.png"];
 let currentIndex = 0;
 
@@ -12,10 +30,18 @@ function updateImage() {
 prevButton.addEventListener("click", () => {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateImage();
+    if (!isMuted) {
+        audio.play();  // Play audio if muted
+        muteButton.innerHTML = '&#x1F50A;';  // Speaker icon
+    }
 });
 
 nextButton.addEventListener("click", () => {
     currentIndex = (currentIndex + 1) % images.length;
     updateImage();
+    if (!isMuted) {
+        audio.play();  // Play audio if muted
+        muteButton.innerHTML = '&#x1F50A;';  // Speaker icon
+    }
 });
 
